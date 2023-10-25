f8a93fcd4c450fd939247fdb0c8ab3e1389d9aad
sheilabett <sheilahbett@gmail.com>
2023-10-25 10:42:21 +0300

    structure of react app

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { format } from "date-fns";
 import ExampleComponent from "./ExampleComponent";
-
+//Okay
 // Add your code own within the return statement
 function App() {
   return (
